80f4d13ae23513726e2a0c913b39a323bea76a66
paschoal <jaqueline.paschoal@scaffoldeducation.com.br>
2018-10-22 08:32:28 -0300

    duck playlists/ api json-server

---
+++ src/store/sagas/playlists.js
@@ -0,0 +1,14 @@
+import api from "../../services/api";
+import { call, put } from "redux-saga/effects";
+
+import { Creators as PlaylistsActions } from "../ducks/playlists";
+
+export function* getPlaylists() {
+  try {
+    const response = yield call(api.get, "/playlists");
+
+    yield put(PlaylistsActions.getPlaylistsSuccess(response.data));
+  } catch (err) {
+    console.log(err);
+  }
+}
